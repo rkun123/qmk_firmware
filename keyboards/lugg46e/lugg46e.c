dbd38ef34792dc8a794f981b6598bcaf7759ff5c
rkun <tren111213@gmail.com>
2023-03-11 12:15:07 +0900

    add: Lugg46e

---
+++ keyboards/lugg46e/lugg46e.c
@@ -0,0 +1,1 @@
+#include "lugg46e.h"
---
+++ keyboards/lugg46e/master/config.h
@@ -0,0 +1,13 @@
+#ifndef MASTER_CONFIG_H_
+#define MASTER_CONFIG_H_
+
+#include "custom_board.h"
+
+#define THIS_DEVICE_ROWS 4
+#define THIS_DEVICE_COLS 6
+
+#define MATRIX_ROW_PINS { PIN11, PIN10, PIN9, PIN8 }
+#define MATRIX_COL_PINS { PIN6, PIN5, PIN4, PIN3, PIN2, PIN1 }
+#define IS_LEFT_HAND  true
+
+#endif /* MASTER_CONFIG_H_ */
